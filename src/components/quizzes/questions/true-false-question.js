84603603429ecae399d092b8956c572dd36b3825
jiazhentang <tang.jiaz@husky.neu.edu>
2021-04-13 15:29:19 -0700

    fixed issues

--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -1,12 +1,67 @@
-import React from "react";
+// import React from "react";
+//
+// const TrueFalseQuestion = ({question}) => {
+//     return(
+//         <div>
+//             <h5>{question.question}</h5>
+//             <label><input type="radio" name={question._id}/> True</label>
+//             <label><input type="radio" name={question._id}/> False</label>
+//             <hr/>
+//         </div>
+//     )
+// }
+//
+// export default TrueFalseQuestion;
+
+import React, {useState} from "react";
 
 const TrueFalseQuestion = ({question}) => {
-    return(
+    const [yourAnswer, setYourAnswer] = useState("")
+    return (
         <div>
-            <h5>{question.question}</h5>
-            <label><input type="radio" name={question._id}/> True</label>
-            <label><input type="radio" name={question._id}/> False</label>
-            <hr/>
+            <h5>{question.question}
+                {
+                    question.correct === yourAnswer &&
+                    <i className="fas fa-check"></i>
+                }
+                {
+                    question.correct !== yourAnswer &&
+                    <i className="fas fa-times"></i>
+                }
+            </h5>
+            <ul className="list-group">
+                <li className={`list-group-item 
+                ${question.correct === yourAnswer ? 'list-group-item-success' : 'list-group-item-danger'}`}>
+                    <label><input
+                        onClick={() => {
+                            setYourAnswer("true")
+                        }}
+                        type="radio"
+                        name={question._id}/>
+                        {''}
+                        True
+                    </label>
+                </li>
+
+                <li className={`list-group-item 
+                ${question.correct === yourAnswer ? 'list-group-item-success' : 'list-group-item-danger'}`}>
+                    <label><input
+                        onClick={() => {
+                            setYourAnswer("false")
+                        }}
+                        type="radio"
+                        name={question._id}/>
+                        {''}
+                        False
+                    </label>
+                </li>
+            </ul>
+            <p>
+                Your answer: {yourAnswer}
+            </p>
+            <button className="btn btn-success">
+                Grade
+            </button>
         </div>
     )
 }
